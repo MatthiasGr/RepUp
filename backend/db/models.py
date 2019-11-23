b733f3419fdd27bba3dcebe6a8528b4f44e48c32
Duc Anh <leducanh235@gmail.com>
2019-11-23 15:12:16 +0100

    Before something worse happens

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, ForeignKey, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
-
 
 class AchievementReg(Base):
     userID = Column(Integer, ForeignKey('points.userID'), primary_key=True)
     achievementID = Column(Integer, ForeignKey('achievements.achievementID'), primary_key=True)
-    time_of_achievement = Column(DateTime)
+    time_of_achievement = Column(DateTime, default=datetime.utcnow)
 
     __tablename__ = 'achievementReg'
 
@@ -19,16 +20,19 @@ class PointsReg(Base):
     userID = Column(Integer, primary_key=True)
     points = Column(Integer)
 
-    __tablename__ = 'points'
+    achievements = relationship('AchievementReg', backref='user')
+    pendings = relationship('WaitingReg', backref='pending_issue')
+
+    __tablename__ = 'pointsReg'
 
     def __repr__(self):
         return f'<User {self.userID}>'
 
 
 class WaitingReg(Base):
-    userID = Column(Integer, ForeignKey('points.userID'), primary_key=True)
+    userID = Column(Integer, ForeignKey('pointsReg.userID'), primary_key=True)
     pending_issueID = Column(Integer)
-    time_of_pending = Column(DateTime)
+    time_of_pending = Column(DateTime, default=datetime.utcnow)
 
     __tablename__ = 'pendingIssues'
 
@@ -39,6 +43,8 @@ class WaitingReg(Base):
 class AchievementCatalog(Base):
     achievementID = Column(Integer, primary_key=True)
     # logo TODO
+
+    achieved = relationship('AchievementReg', backref='achievement')
 
     __tablename__ = 'achievements'
 
