f57eb84b86752c910960710496922ff3ac0b9ed3
Duc Anh <leducanh235@gmail.com>
2019-11-23 21:16:26 +0100

    Migrated with reaally reaaally fishy fix, absolutely horrible

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,13 +1,11 @@
 __all__ = ["config", "helper", "models"]
 
-from flask import Flask, make_response
+#from flask import Flask, make_response
 from db.config import Config
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+#app = Flask(__name__)
 
-from db import models
-from db.models import *
-from db.helper import *
+#from db import models
+#from db.models import *
+#from db.helper import *
